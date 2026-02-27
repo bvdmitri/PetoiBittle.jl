4a4b931889b0978e85b6378febd72f310cc7f702
bvdmitri <bvdmitri@gmail.com>
2026-02-27 09:01:14 +0100

    Improve the documenation build

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,19 @@
 using PetoiBittle
 using Documenter
 
-DocMeta.setdocmeta!(PetoiBittle, :DocTestSetup, :(using PetoiBittle); recursive = true)
+DocMeta.setdocmeta!(PetoiBittle, :DocTestSetup, :(using PetoiBittle); recursive=true)
 
 makedocs(;
-    modules = [PetoiBittle],
-    authors = "Dmitry Bagaev <bvdmitri@gmail.com> and contributors",
-    sitename = "PetoiBittle.jl",
-    format = Documenter.HTML(;
-        canonical = "https://bvdmitri.github.io/PetoiBittle.jl", edit_link = "main", assets = String[]
+    modules=[PetoiBittle],
+    authors="Dmitry Bagaev <bvdmitri@gmail.com> and contributors",
+    sitename="PetoiBittle.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://bvdmitri.github.io/PetoiBittle.jl",
+        edit_link="main",
+        assets=String[]
     ),
-    pages = ["Home" => "index.md", "Commands" => "commands.md"]
+    pages=["Home" => "index.md", "Commands" => "commands.md"]
 )
 
-deploydocs(; repo = "github.com/bvdmitri/PetoiBittle.jl", devbranch = "main")
+deploydocs(; repo="github.com/bvdmitri/PetoiBittle.jl", devbranch="main")
--- docs/src/commands.md
+++ docs/src/commands.md
@@ -7,17 +7,21 @@ PetoiBittle.Command
 PetoiBittle.send_command
 ```
 
-## The list of available commands
+# The list of available commands
 
 The full list of available commands for Petoi Robots and their specification can also be found on the Petoi Bittle website in the [Serial Protocol](https://docs.petoi.com/apis/serial-protocol) section. This library implements a **subset** of commands. PRs are welcome to add new commands!
 
-Use
-```julia
-julia> subtypes(PetoiBittle.Command)
+To get the list of all available commands implemented in the package use in REPL:
+```@repl list-of-available-commands
+using PetoiBittle #hide
+using InteractiveUtils #hide
+subtypes(PetoiBittle.Command)
 ```
-to get the list of all available commands implemented in the package.
 
-### Move joints
+!!! note
+    This command only works in Julia REPL.
+
+## Move joints
 
 ```@docs
 PetoiBittle.MoveJoints
